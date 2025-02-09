085467a54930d259e0d46d47f2f6daf761a43c6a
Piyush Kumar <piyushkumar1116@gmail.com>
2025-02-09 20:53:14 +0530

    fix uvicorn

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from typing import Union
-from mangum import Mangum
 
 # Initialize FastAPI
 app = FastAPI()
@@ -13,7 +12,6 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-# Create handler for Vercel
-handler = Mangum(app, lifespan="off")  # Add lifespan="off"
+
 
 #  pip freeze > requirements.txt
